d7840a4fb91036ad854a1a7f3ae280b4f622c425
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2021-10-13 00:27:40 +0530

    API Added

---
+++ routes/api/v2/comments.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+const commentsApi = require('../../../controllers/api/v2/comment_api');
+
+router.get('/', commentsApi.index);
+
+module.exports = router;

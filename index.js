b724920cdc1f06551afa86c1e24196d5896379e6
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2021-10-14 13:55:49 +0530

    CSS updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const pssportJwt = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
 const MongoStore = require('connect-mongo')(session);
 const { param } = require('./routes');
 const sassMiddleware = require('node-sass-middleware');
